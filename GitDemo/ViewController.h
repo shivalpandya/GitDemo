a6a878475d11d47d9da7134f82ecb3c5759807bb
Shival <webwerks@Shival.local>
2013-03-18 14:23:30 +0530

    Initial Commit

---
+++ GitDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by Shival on 18/03/13.
+//  Copyright (c) 2013 shival. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by Shival on 18/03/13.
+//  Copyright (c) 2013 shival. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
